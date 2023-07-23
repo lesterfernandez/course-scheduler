5b3b75feb01cb4147a1117ac0433868ff8700b97
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-07-23 16:11:02 -0700

    feat(backend): allow custom db url

---
+++ backend/data/db.go
@@ -0,0 +1,27 @@
+package data
+
+import (
+	"os"
+
+	"github.com/lesterfernandez/course-scheduler/backend/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func SetupDb() *gorm.DB {
+	url := os.Getenv("DATABASE_URL")
+	if url == "" {
+		url = "postgresql://postgres:postgres@localhost:5001"
+	}
+
+	db, err := gorm.Open(postgres.Open(url),
+		&gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+	if err != nil {
+		panic("Could not connect to db")
+	}
+	db.AutoMigrate(&model.User{}, &model.Course{})
+	return db
+}
--- backend/main.go
+++ backend/main.go
@@ -6,15 +6,11 @@ import (
 
 	"github.com/lesterfernandez/course-scheduler/backend/data"
 	"github.com/lesterfernandez/course-scheduler/backend/handle"
-	"github.com/lesterfernandez/course-scheduler/backend/model"
 	"github.com/rs/cors"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 func main() {
-	db := setupDb()
+	db := data.SetupDb()
 
 	user := &data.UserData{Db: db}
 	course := &data.CourseData{Db: db}
@@ -26,18 +22,6 @@ func main() {
 	c := setupCors()
 
 	log.Panicln(http.ListenAndServe(":8080", c.Handler(http.DefaultServeMux)))
-}
-
-func setupDb() *gorm.DB {
-	db, err := gorm.Open(postgres.Open("postgresql://postgres:postgres@localhost:5001"),
-		&gorm.Config{
-			Logger: logger.Default.LogMode(logger.Info),
-		})
-	if err != nil {
-		panic("Could not connect to db")
-	}
-	db.AutoMigrate(&model.User{}, &model.Course{})
-	return db
 }
 
 func setupCors() *cors.Cors {
