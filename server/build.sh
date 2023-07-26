3eeb7f6308b97e2e05b857b1ee129a0acbae8407
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-07-25 23:43:37 -0700

    chore: delete legacy java backend

--- server/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-./mvnw clean compile package
-
-docker buildx build \
---push \
---platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
--t lesterfernandez/course-scheduler:1.0 .
-
--- server/src/main/java/me/lesterfernandez/CourseScheduler/course/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package me.lesterfernandez.CourseScheduler.course;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends CrudRepository<Course, Long> {
-
-}
